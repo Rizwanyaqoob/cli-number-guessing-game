a20af357e1187473d466b3f8237bb087d7f785bf
Rizwanyaqoob <160166535+Rizwanyaqoob@users.noreply.github.com>
2024-03-31 16:20:23 -0700

    Add files via upload
    create number guessing game

---
+++ cli-number-game/README.md
@@ -0,0 +1,2 @@
+# cli-number-game
+This is cli number guessng game based on typescript
---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt({
+    name: "userGuessNumber",
+    type: "number",
+    message: "Please guess a number b/w 1-10:",
+});
+if (answer.userGuessNumber === random) {
+    console.log("Coongratulation ! You guess Right Number");
+}
+else {
+    console.log("Ohh! sorry , you Guess Wrong Number.");
+}
+console.log(random);
